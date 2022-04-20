6475d95f1d68656a8278bc5eab399cd632d92dac
Fredrik Eriksson <eriksson.nf@gmail.com>
2022-04-20 10:37:13 +0200

    Add serviceLoader

---
+++ Consumer/src/main/java/consumer/Main.java
@@ -0,0 +1,18 @@
+package consumer;
+
+import greet.Greeting;
+
+import java.util.ServiceLoader;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ServiceLoader<Greeting> serviceLoader = ServiceLoader.load(Greeting.class);
+
+        for (Greeting greeting : serviceLoader) {
+            System.out.println(greeting.greet());
+        }
+
+
+    }
+}
